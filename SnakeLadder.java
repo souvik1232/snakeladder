a50673a22ee838c4ce0c31fda0e2cbdf293450fb
souvik1232 <souvik199831@gmail.com>
2020-12-14 19:10:51 +0530

    Added:Snake and Ladder game initialised

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.java.snakeladder;
+
+public class SnakeLadder {
+   // public static final int PLAYER_ONE_POSITION = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+    }
+}
