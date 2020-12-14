2bb26e38ea32dd049e236c01d73c5f50582351fe
souvik1232 <souvik199831@gmail.com>
2020-12-14 19:39:51 +0530

    Added:Randomise the playing options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,12 +3,28 @@ package com.java.snakeladder;
 import java.util.Random;
 
 public class SnakeLadder {
-    int playerOnePosition = 0;
+    public static int playerOnePosition = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         Random random = new Random();
         int diceRoll = random.nextInt(7);
-        System.out.println(diceRoll);
+        int option = random.nextInt(3);
+        System.out.println("Dice Roll is : "+diceRoll);
+        switch (option) {
+            case 0:
+                System.out.println("No Play");
+                break;
+            case 1:
+                System.out.println("Ladder");
+                playerOnePosition += diceRoll;
+                break;
+            case 2:
+                System.out.println("Snake");
+                playerOnePosition -= diceRoll;
+                break;
+            default:
+                System.out.println("-----");
+        }
     }
 }
