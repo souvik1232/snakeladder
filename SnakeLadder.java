dd3377df519ba475300b594f5fd911c1a37d82c7
souvik1232 <souvik199831@gmail.com>
2020-12-14 19:27:53 +0530

    Added:Rolling Dice initialised

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,14 @@
 package com.java.snakeladder;
 
+import java.util.Random;
+
 public class SnakeLadder {
-    public static final int PLAYER_ONE_POSITION = 0;
+    int playerOnePosition = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
+        Random random = new Random();
+        int diceRoll = random.nextInt(7);
+        System.out.println(diceRoll);
     }
 }
