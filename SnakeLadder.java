87d2f9213fb2911460496013d6527aaaa1480afa
souvik1232 <souvik199831@gmail.com>
2020-12-14 19:55:53 +0530

    Added:Logic for players movement

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,23 +8,28 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
         Random random = new Random();
-        int diceRoll = random.nextInt(7);
-        int option = random.nextInt(3);
-        System.out.println("Dice Roll is : "+diceRoll);
-        switch (option) {
-            case 0:
-                System.out.println("No Play");
-                break;
-            case 1:
-                System.out.println("Ladder");
-                playerOnePosition += diceRoll;
-                break;
-            case 2:
-                System.out.println("Snake");
-                playerOnePosition -= diceRoll;
-                break;
-            default:
-                System.out.println("-----");
-        }
+        do {
+            int diceRoll = random.nextInt(7);
+            int option = random.nextInt(3);
+            System.out.println("Dice Roll is : "+diceRoll);
+            switch (option) {
+                case 0:
+                    System.out.println("No Play");
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    playerOnePosition += diceRoll;
+                    break;
+                case 2:
+                    System.out.println("Snake");
+                    playerOnePosition -= diceRoll;
+                    break;
+                default:
+                    System.out.println("-----");
+            }
+            if (playerOnePosition < 0)
+                playerOnePosition = 0;
+            System.out.println("Player one Position --> " +playerOnePosition);
+        }while (playerOnePosition <= 100);
     }
 }
